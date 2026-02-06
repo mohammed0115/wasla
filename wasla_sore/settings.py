e3b03dec74a71a5af6a27b03b110472ec85dd6b0
mohammed0115 <mohemmedkamal0115498898@gmail.com>
2026-02-06 23:50:18 +0300

    first commit

--- wasla_sore/settings.py
+++ wasla_sore/settings.py
@@ -214,3 +214,7 @@ SMS_PROVIDERS = {
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "static"
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
